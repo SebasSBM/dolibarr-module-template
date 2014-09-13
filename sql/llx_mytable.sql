56cfcea89dabfe4dbc5c499ac21b0068d7fc9075
Sebastian <sebassbm.info@gmail.com>
2014-09-13 19:44:13 +0200

    Update llx_mytable.sql

--- sql/llx_mytable.sql
+++ sql/llx_mytable.sql
@@ -14,7 +14,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-create table llx_provfunds
+create table llx_mytable
 (
         rowid       integer AUTO_INCREMENT  PRIMARY KEY,
 
